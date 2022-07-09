37581f181685db299222288a9cf7e8ceb3ea7530
WilDevp <wilmar.garciava@gmail.com>
2022-07-09 04:18:05 +0200

    Add constructor to text call of beans in project ob-spring-helloworld

--- spring/ob-spring-helloworld/src/main/java/com/example/CalculatorService.java
+++ spring/ob-spring-helloworld/src/main/java/com/example/CalculatorService.java
@@ -2,6 +2,10 @@ package com.example;
 
 public class CalculatorService {
 
+    public CalculatorService() {
+        System.out.println("Text constructor CalculatorService");
+    }
+
     public String holaMundo(){
         return "Hola Mundo";
     }
