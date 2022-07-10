e213286c5269cc528f1834cdcd0973909023091c
WilDevp <wilmar.garciava@gmail.com>
2022-07-11 00:28:22 +0200

    Add new bean with dependency and another bean

--- spring/Beans/ob-spring-helloworld/src/main/java/com/example/GestorFacturas.java
+++ spring/Beans/ob-spring-helloworld/src/main/java/com/example/GestorFacturas.java
@@ -5,11 +5,13 @@ public class GestorFacturas {
     //Atributos
 
     CalculatorService calculatorService;
+    String nombre;
 
     //Constructor
-    public GestorFacturas(CalculatorService calculatorService) {
+    public GestorFacturas(CalculatorService calculatorService, String nombre) {
         System.out.println("Ejecutando constructor de facturas");
         this.calculatorService = calculatorService;
+        this.nombre = nombre;
     }
 
     //Metodos
--- spring/Beans/ob-spring-helloworld/src/main/java/com/example/Main.java
+++ spring/Beans/ob-spring-helloworld/src/main/java/com/example/Main.java
@@ -7,13 +7,26 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml"); //Agregamos el container para llamar los beans
 
+        //Ejemplo 1
         //Opciòn 1: Crear un objeto de una forma normal
-        CalculatorService calculatorService  = new CalculatorService();
+        //CalculatorService calculatorService  = new CalculatorService();
 
         //Opciòn 2: Recibir un objeto de Spring
         CalculatorService service = (CalculatorService) context.getBean("calculatorService"); //Hacemos el llamado al bean, el cual crearà el objeto y lo inyectamos
 
         String texto = service.holaMundo();
         System.out.println(texto);
+
+        //Se puede volver a obtener
+        //OJO: se recupera el mismo objeto
+
+        CalculatorService service2 = (CalculatorService) context.getBean("calculatorService"); //Hacemos el llamado al bean, el cual crearà el objeto y lo inyectamos
+
+        String texto2 = service.holaMundo();
+        System.out.println(texto2);
+
+        //Ejemplo 2
+        GestorFacturas gestorFacturas = (GestorFacturas) context.getBean("gestorFacturas");
+        System.out.println(gestorFacturas.calculatorService.holaMundo());
     }
 }
