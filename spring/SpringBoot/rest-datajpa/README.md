79425b5d8c5a8efef2ca9b80c8f7cb1d1cfdee4a
WilDevp <wilmar.garciava@gmail.com>
2022-07-13 12:07:42 +0200

    Project SpringBoot add model with class Book with structure

---
+++ spring/SpringBoot/rest-datajpa/README.md
@@ -0,0 +1,12 @@
+## Spring Boot
+
+proyecto Spring Boot con las dependencias / startes:
+
+* H2
+* Spring Data JPA
+* Spring Web
+* Spring Boot Dev Tools
+
+Aplicación Api Rest con acceso a base de datos de H2 para persistir la información.
+
+El acceso se puede realizar desde un programa como Postma, Insomnia etc...
---
+++ spring/SpringBoot/rest-datajpa/src/main/java/com/example/restdatajpa/model/Book.java
@@ -0,0 +1,103 @@
+package com.example.restdatajpa.model;
+
+import java.time.LocalDate;
+
+public class Book {
+    //Atributos
+    private Long id;
+    private String title;
+    private String autor;
+    private Integer pages;
+    private Double price;
+    private LocalDate realeaseDate;
+    private boolean online;
+
+    //Constructores
+
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String autor, Integer pages, Double price, LocalDate realeaseDate, boolean online) {
+        this.id = id;
+        this.title = title;
+        this.autor = autor;
+        this.pages = pages;
+        this.price = price;
+        this.realeaseDate = realeaseDate;
+        this.online = online;
+    }
+
+    //Getter y Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getRealeaseDate() {
+        return realeaseDate;
+    }
+
+    public void setRealeaseDate(LocalDate realeaseDate) {
+        this.realeaseDate = realeaseDate;
+    }
+
+    public boolean isOnline() {
+        return online;
+    }
+
+    public void setOnline(boolean online) {
+        this.online = online;
+    }
+
+    //ToString
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", autor='" + autor + '\'' +
+                ", pages=" + pages +
+                ", price=" + price +
+                ", realeaseDate=" + realeaseDate +
+                ", online=" + online +
+                '}';
+    }
+}
