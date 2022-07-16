cca86f315957e290e8ea2200146d0bb9cedd3a3a
WilDevp <wilmar.garciava@gmail.com>
2022-07-16 21:37:37 +0200

    Add save()

--- spring/SpringBoot/Ejercicios/src/main/java/com/example/ejercicios/controller/LaptopController.java
+++ spring/SpringBoot/Ejercicios/src/main/java/com/example/ejercicios/controller/LaptopController.java
@@ -3,6 +3,8 @@ package com.example.ejercicios.controller;
 import com.example.ejercicios.entity.Laptop;
 import com.example.ejercicios.repository.LaptopRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +19,15 @@ public class LaptopController {
         this.laptopRepository = laptopRepository;
     }
 
+    //List all lapatops
     @GetMapping("/api/laptops")
     public List<Laptop> findAll(){
         return laptopRepository.findAll();
+    }
+
+    //Save laptop
+    @PostMapping("/api/laptops")
+    public Laptop save(@RequestBody Laptop laptop){
+        return laptopRepository.save(laptop);
     }
 }
